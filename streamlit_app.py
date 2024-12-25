3d928d35ef372d16414a408a425d7a3889de9f6d
Sasikala Kesavan <57978031+sasikala-kesavan@users.noreply.github.com>
2024-12-25 06:56:40 +0000

    Added OPENAI Key

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,9 @@ import openai
 import streamlit as st
 
 with st.sidebar:
-    st.title('🤖💬 OpenAI Chatbot')
-    if 'OPENAI_API_KEY' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        openai.api_key = st.secrets['OPENAI_API_KEY']
-    else:
-        openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
-        if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-
+    st.title('Ericsson EOC Quotation Creation Bot')
+    openai.api_key = st.secrets['general']['OPENAI_API_KEY']
+    
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
