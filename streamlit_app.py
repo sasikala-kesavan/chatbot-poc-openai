74c84de5e1cfb63bb418c59d300a2af3d3555921
Sasikala Kesavan <57978031+sasikala-kesavan@users.noreply.github.com>
2024-12-27 08:16:18 +0000

    shopping-cart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,9 @@
 # Code refactored from https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps
-
-from openai import OpenAI
+import openai
 import streamlit as st
 
-# Streamlit app title
-st.title('Ericsson EOC Quotation Creation Bot')
-# Set your OpenAI API key (you can also set it in environment variables for security)
-client = OpenAI(
-    api_key=st.secrets['general']['OPENAI_API_KEY'],  # This is the default and can be omitted
-)
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Say this is a test",
-        }
-    ],
-    model="gpt-4o",
-)
+st.sidebar.header(":shopping_trolley: Shopping Cart")
+
+st.markdown("<h3>Exec Go - Guided Selling with Enterprise Chatbot</h3>",
+            unsafe_allow_html=True)
